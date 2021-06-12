4064b8bfb3dd35b3555af7925bff2e5306e5d199
Toni Ruoranen <Toni.Ruoranen@metropolia.fi>
2021-06-12 11:24:49 +0300

    First commit to create repo, only examples still

---
+++ Lataaja.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+
+from pytrends.request import TrendReq
+
+pytrend = TrendReq()
+
+pytrend.build_payload(kw_list=['depression','exercise'])
+
+
+interest_over_time_df = pytrend.interest_over_time()
+print(interest_over_time_df.head())
+
+# Get Google Keyword Suggestions
+# suggestions_dict = pytrend.suggestions(keyword='sad')
+# print(suggestions_dict)
+
+
+
+# related_queries_dict = pytrend.related_queries()
+# print(related_queries_dict)
